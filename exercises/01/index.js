865b40b26446e8fcef9e7eb565ac53343521baeb
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-01-17 00:02:04 +0200

    First exercise done

---
+++ exercises/01/index.js
@@ -0,0 +1,8 @@
+const h1DOM = document.querySelector('h1');
+h1DOM.textContent = "Labas vakaras!";
+
+const paragraphDOM = document.querySelector('p');
+paragraphDOM.textContent = "Šiandien gražus oras!";
+
+const linkDOM = document.querySelector('a');
+linkDOM.textContent = "Spausk mane!";
