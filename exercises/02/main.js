e822b9ef7666cc98ec45184947a7fcac644f3090
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-01-17 01:03:54 +0200

    fix missed requirement

--- exercises/02/main.js
+++ exercises/02/main.js
@@ -2,7 +2,7 @@ const ingredientsDOM = document.querySelectorAll("li")
 const ingredientsArray = []
 for (const item of ingredientsDOM) {
     console.log(item.textContent)
-    ingredientsArray.push(item.textContent)
+    ingredientsArray.push(item.textContent.toUpperCase())
 }
 
 console.log(ingredientsArray)
